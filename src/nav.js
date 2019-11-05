e37542f80ab554cafa02b948d1121f57971b0dd9
GerardoHP <verdaderoperrodelmal@hotmail.com>
2019-11-05 17:24:30 -0600

    Completed module 07

--- src/nav.js
+++ src/nav.js
@@ -1,20 +1,31 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from "prop-types";
+import React from "react";
+import styleable from "react-styleable";
 
-function Nav(props) {
+import css from "./nav.module.css";
+
+function Nav({ onPrevious, onNext, hasPrevious, hasNext, css }) {
+  const getPrevClassName = () => (hasPrevious ? css.prev : css.prevHidden);
+  const getNextClassName = () => (hasNext ? css.next : css.nextHidden);
+
   return (
-    <div>
-      <button onClick={props.onPrevious}>&#10094;</button>
-      <button onClick={props.onNext}>&#10095;</button>
+    <div className={css.root}>
+      <button className={getPrevClassName()} onClick={onPrevious}>
+        &#10094;
+      </button>
+      <button className={getNextClassName()} onClick={onNext}>
+        &#10095;
+      </button>
     </div>
-  )
+  );
 }
 
 Nav.propTypes = {
   onPrevious: PropTypes.func.isRequired,
   onNext: PropTypes.func.isRequired,
   hasPrevious: PropTypes.bool,
-  hasNext: PropTypes.bool
-}
+  hasNext: PropTypes.bool,
+  css: PropTypes.object
+};
 
-export default Nav
+export default styleable(css)(Nav);
