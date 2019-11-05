d7624306f1f4e555c603602a8d9d1809b5130ec7
Gerardo Hernandez <gerardojhp@gmail.com>
2019-11-05 00:16:09 -0600

    Finished module 06

--- src/nav.js
+++ src/nav.js
@@ -1,11 +1,24 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import classnames from 'classnames';
 
 function Nav(props) {
+
+  const getPrevClassNames = () => classnames('dft__nav__btn dft__nav__btn--prev', { 'dft__nav__btn--hidden': !props.hasPrevious });
+  const getNextClassNames = () => classnames('dft__nav__btn dft__nav__btn--next', { 'dft__nav__btn--hidden': !props.hasNext });
+
   return (
-    <div>
-      <button onClick={props.onPrevious}>&#10094;</button>
-      <button onClick={props.onNext}>&#10095;</button>
+    <div className="dft__nav">
+      <button
+        className={getPrevClassNames()}
+        onClick={props.onPrevious}>
+        &#10094;
+        </button>
+      <button
+        className={getNextClassNames()}
+        onClick={props.onNext}>
+        &#10095;
+         </button>
     </div>
   )
 }
